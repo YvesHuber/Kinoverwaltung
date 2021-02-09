ed3f6eaf38b5426df39750a2e3485c906042d2b0
ale05ZLI <alex_m_s@yahoo.com>
2021-02-09 13:50:50 +0100

    picture chair

--- php/connection.class.php
+++ php/connection.class.php
@@ -6,7 +6,6 @@ $connection = new mysqli('localhost', 'root', 'root', 'kino','3306');
 if ($connection->connect_error) {
     die( "Failed to connect to MySQL: " . $connection->connect_error);
     
+}else {
+    echo "Connection successful";
 }
-    else {
-        echo "Connection successful";
-    }
