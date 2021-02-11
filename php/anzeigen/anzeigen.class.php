756fc9bb3ade1fed771e652493ab4be1ab4c3c34
Yves Huber <yves.huber05@gmail.com>
2021-02-11 14:43:36 +0100

    added copie

--- php/anzeigen/anzeigen.class.php
+++ php/anzeigen/anzeigen.class.php
@@ -49,35 +49,40 @@ class Visualise
     public function choose($num)
     {
         echo "<br>";
-        $userfs = "SELECT sitze FROM saal_plätze WHERE id = $num ; ";
-        $querys = mysqli_query($this->connection, $userfs);
-        $rows = mysqli_fetch_assoc($querys);
-        $id = $rows['sitze'];
+            $userfs = "SELECT sitze FROM saal_plätze WHERE id = $num ; ";
+            $querys = mysqli_query($this->connection, $userfs);
+            $rows = mysqli_fetch_assoc($querys);
+            $id = $rows['sitze'];
 
-        $bool = "SELECT platz_nummer, saal_plätze_id_fs FROM saal WHERE besetzt = 't' AND saal_plätze_id_fs = $num ;";
-        $query_bool = mysqli_query($this->connection, $bool);
-        $row = mysqli_fetch_assoc($query_bool);
-        //var_dump($row);
+            $bool = "SELECT platz_nummer, saal_plätze_id_fs FROM saal WHERE besetzt = 't' AND saal_plätze_id_fs = $num ;";
+            $query_bool = mysqli_query($this->connection, $bool);
+            $row = mysqli_fetch_assoc($query_bool);
+            //var_dump($row);
 
-        for ($p = 1; $p <= $id; $p++) {
-            if ($row['saal_plätze_id_fs'] == "$num" && $row['platz_nummer'] == "$p") {
+            for ($p = 1; $p <= $id; $p++) {
+                if ($row['saal_plätze_id_fs'] == "$num" && $row['platz_nummer'] == "$p") {
 
-                echo "<img class = 'seat' src = ../../Bilder/stuhl.svg>";
-                echo "<t style='color: red;'>" . $p . "</t>";
-                if ($p % 10 == 0) {
-                    echo "<br>";
-                }
-            } else {
-                echo "<img class = 'seat' src = ../../Bilder/stuhl.svg>";
-                echo "<t class = seatid style='color: white;'>" . $p . "</t>";
-                if ($p % 10 == 0) {
-                    echo "<br>";
+                    echo "<img class = 'seat' src = ../../Bilder/stuhl.svg>";
+                    echo "<t style='color: red;'>" . $p . "</t>";
+                    if ($p % 10 == 0) {
+                        echo "<br>";
+                    }
+                } else {
+                    echo "<img class = 'seat' src = ../../Bilder/stuhl.svg>";
+                    echo "<t class = seatid style='color: white;'>" . $p . "</t>";
+                    if ($p % 10 == 0) {
+                        echo "<br>";
+                    }
                 }
             }
-        }
-        echo "<br>";
-        echo "<br>";
-        echo "<br>";
-        $p = 1;
+            echo "<br>";
+            echo "<br>";
+            echo "<br>";
+            $p = 1;
+    }
+
+    public function autodelete()
+    {
+
     }
 }
