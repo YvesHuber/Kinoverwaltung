728161ee2915df485b1910cf02cf3d0e5dbfb559
Yves Huber <yves.huber05@gmail.com>
2021-02-12 09:25:55 +0100

    Fixed Admin show

--- php/anzeigen/anzeigen.class.php
+++ php/anzeigen/anzeigen.class.php
@@ -27,39 +27,42 @@ class Visualise
     {
         $idsaal = 3;
         for ($i = 1; $i <= $idsaal; $i++) {
-            echo $i;
-            $userfs = "SELECT sitze FROM saal_plätze WHERE id = $i ; ";
-            $querys = mysqli_query($this->connection, $userfs);
-            $rows = mysqli_fetch_assoc($querys);
-            $id = $rows['sitze'];
-
-            $bool = "SELECT platz_nummer, saal_plätze_id_fs FROM saal WHERE besetzt = 't' AND saal_plätze_id_fs = $i ;";
-            $query_bool = mysqli_query($this->connection, $bool);
-            $row = mysqli_fetch_assoc($query_bool);
-            var_dump($row);
-
-            for ($p = 1; $p <= $id; $p++) {
-                if ($row['saal_plätze_id_fs'] == "$i" && $row['platz_nummer'] == "$p") {
-
-                    echo "<img style = width = 4% height = 4% src = ../../Bilder/stuhl.svg>";;
-                    echo "<t style='color: red;'>" . $p . "</t>";
-                    if ($p % 10 == 0) {
-                        echo "<br>";
-                    }
-                } else {
-                    echo "<img style = width = 4% height = 4% src = ../../Bilder/stuhl.svg>";
-                    echo "<t class = seatid style='color: white;'>" . $p . "</t>";
-                    if ($p % 10 == 0) {
-                        echo "<br>";
-                    }
+            $array = array();
+        echo "<br>";
+        $userfs = "SELECT sitze FROM saal_plätze WHERE id = $i ; ";
+        $querys = mysqli_query($this->connection, $userfs);
+        $rows = mysqli_fetch_assoc($querys);
+        $id = $rows['sitze'];
+        $bool = "SELECT platz_nummer, saal_plätze_id_fs FROM saal WHERE besetzt = 't' AND saal_plätze_id_fs = $i ;";
+        $query_bool = mysqli_query($this->connection, $bool);
+        if ($query_bool) {
+            if (mysqli_num_rows($query_bool) > 0) {
+                while ($row = mysqli_fetch_assoc($query_bool)) {
+                    $array[] =  intval($row["platz_nummer"]);
                 }
             }
-            echo "<br>";
-            echo "<br>";
-            echo "<br>";
-            $p = 1;
         }
+        $p = 1;
+        $count = 0;
+        do {
+            echo "<img class = 'seat' src = ../../Bilder/stuhl.svg>";
+            if ($array[$count] == $p) {
+                echo "<t style='color: red;'>" . $p . "</t>";
+                $count++;
+            } else {
+                echo "<t class = seatid style='color: white;'>" . $p . "</t>";
+            }
+            if ($p % 10 == 0) {
+                echo "<br>";
+            }
+            $p++;
+        } while ($p <= $id);
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
+        $p = 1;
     }
+}
 
     /**
      * choose
@@ -85,7 +88,7 @@ class Visualise
             }
         }
         $p = 1;
-        $count=0;
+        $count = 0;
         do {
             echo "<img class = 'seat' src = ../../Bilder/stuhl.svg>";
             if ($array[$count] == $p) {
